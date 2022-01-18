da5a3b1aed55f5326671f696ee30b8e19d3ce0ad
KhanbalaRashidov <reshidovxanbala@gmail.com>
2022-01-18 14:08:50 +0300

    Implement Orders Service

--- ECommerce.Api.Search/Program.cs
+++ ECommerce.Api.Search/Program.cs
@@ -1,7 +1,14 @@
+using ECommerce.Api.Search.Interfaces;
+using ECommerce.Api.Search.Services;
+
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
+builder.Services.AddScoped<ISearchService, SearchService>();
+builder.Services.AddScoped<IOrdersService, OrdersService>();
+builder.Services.AddHttpClient("OrdersService", config =>
+{
+    config.BaseAddress = new Uri(builder.Configuration["Services:Orders"]);
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ ECommerce.Api.Search/Services/SearchService.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.Api.Search.Interfaces;
+
+namespace ECommerce.Api.Search.Services
+{
+    public class SearchService : ISearchService
+    {
+        private readonly IOrdersService _ordersService;
+        public SearchService(IOrdersService ordersService)
+        {
+            _ordersService = ordersService;
+        }
+        public async Task<(bool IsSucccess, dynamic SearchResult)> SearchAsync(int customerId)
+        {
+            var ordersResult = await _ordersService.GetOrdersAsync(customerId);
+            if (ordersResult.IsSuccess)
+            {
+                var result = new
+                {
+                    Orders = ordersResult.Orders,
+                };
+                return (true, result);
+            }
+            return (false, null);
+        }
+    }
+}
